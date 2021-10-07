a0845955474090deefb50f600777a15a45b18aaa
Ezequiel <ezequiel1de1oliveira@gmail.com>
2021-10-07 14:55:24 -0300

    Adding cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const routes = require('./routes');
+const cors = require('cors');
+
 const { connectDB } = require('./db')
 
 const { PORT, NODE_ENV } = process.env;
@@ -15,6 +16,7 @@ connectDB()
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => {
